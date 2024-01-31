a9d20b9c85b2380f8e3220a1cf412efa9d245612
Thomas Thaler <thomas.thaler96@hotmail.de>
2024-01-31 12:54:19 +0200

    added location to ast nodes

--- src/ast.mjs
+++ src/ast.mjs
@@ -1,63 +1,80 @@
-export function Literal(value){
+export function Expression(loc){
+    this.loc = loc
+}
+
+export function Literal(value, loc){
+    Expression.call(this, loc)
     this.value = value
 }
 
-export function Identifier(name){
+export function Identifier(name, loc){
+    Expression.call(this, loc)
     this.name = name
 }
 
-export function BinaryExpr(left, op, right){
+export function BinaryExpr(left, op, right, loc){
+    Expression.call(this, loc)
     this.left = left
     this.op = op
     this.right = right
 }
 
-export function LogicalExpr(left, op, right){
+export function LogicalExpr(left, op, right, loc){
+    Expression.call(this, loc)
     this.left = left
     this.op = op
     this.right = right
 }
 
-export function UnaryExpr(right, op){
+export function UnaryExpr(right, op, loc){
+    Expression.call(this, loc)
     this.right = right
     this.op = op
 }
 
-export function Grouping(expr){
+export function Grouping(expr, loc){
+    Expression.call(this, loc)
     this.expr = expr
 }
 
-export function Assignment(target, expr){
+export function Assignment(target, expr, loc){
+    Expression.call(this, loc)
     this.target = target
     this.expr = expr
 }
 
-export function IfExpr(cond, body, elsz){
+export function IfExpr(cond, body, elsz, loc){
+    Expression.call(this, loc)
     this.cond = cond
     this.body = body
     this.elsz = elsz
 }
 
-export function WhileExpr(cond, body){
+export function WhileExpr(cond, body, loc){
+    Expression.call(this, loc)
     this.cond = cond
     this.body = body
 }
 
-export function Call(target, args){
+export function Call(target, args, loc){
+    Expression.call(this, loc)
     this.target = target
     this.args = args
 }
 
-export function Declaration(ident, initializer){
+export function Declaration(ident, initializer, loc){
+    Expression.call(this, loc)
     this.ident = ident
     this.initializer = initializer
 }
 
-export function Block(exprs){
+export function Block(exprs, loc){
+    Expression.call(this, loc)
     this.exprs = exprs
 }
 
-export function TypeExpr(type, expr){
+export function TypeExpr(type, expr, loc){
+    Expression.call(this, loc)
     this.type = type
     this.expr = expr
 }
--- src/parser.mjs
+++ src/parser.mjs
@@ -61,58 +61,58 @@ function Parser(tokens){
     }
 
     this.parseExpression = function(){
-        let expr = this.__parseExpression()
+        const expr = this.__parseExpression(peek().loc)
         expect(TokenType.END, `EOF expected, got ${peek().type}`)
         return expr
     }
-    this.__parseExpression = function(){
-        if (match(TokenType.IF)) return this.parseIfExpression()
-        if (match(TokenType.VAR)) return this.parseVarDeclaration()
-        if (match(TokenType.WHILE)) return this.parseWhileExpression()
-        if (match(TokenType.LBRACE)) return this.parseBlockExpression()
-        return this.parseAssignment()
+    this.__parseExpression = function(loc){
+        if (match(TokenType.IF)) return this.parseIfExpression(loc)
+        if (match(TokenType.VAR)) return this.parseVarDeclaration(loc)
+        if (match(TokenType.WHILE)) return this.parseWhileExpression(loc)
+        if (match(TokenType.LBRACE)) return this.parseBlockExpression(loc)
+        return this.parseAssignment(loc)
     }
-    this.parseIfExpression = function(){
+    this.parseIfExpression = function(loc){
         expect(TokenType.IF, `Expected ${TokenType.IF}, got ${peek().type}`)
-        const cond = this.parseLeftPrecedenceExpr()
+        const cond = this.parseLeftPrecedenceExpr(peek().loc)
         expect(TokenType.THEN, `Expected ${TokenType.THEN}, got ${peek().type}`)
 
-        const body = this.__parseExpression()
+        const body = this.__parseExpression(peek().loc)
         let elsz
         
         if (match(TokenType.ELSE)){
             advance()
-            elsz = this.__parseExpression()
+            elsz = this.__parseExpression(peek().loc)
         }
-        return new IfExpr(cond, body, elsz)
+        return new IfExpr(cond, body, elsz, loc)
     }
-    this.parseVarDeclaration = function(){
+    this.parseVarDeclaration = function(loc){
         expect(TokenType.VAR, `Expected ${TokenType.VAR}, got ${peek().type}`)
-        const ident = this.parseIdentifier()
+        const ident = this.parseIdentifier(peek().loc)
         let type
 
         if (match(TokenType.COLON)){
             advance()
-            type = this.parseIdentifier()
+            type = this.parseIdentifier(peek().loc)
         }
         expect(TokenType.EQ, `Expected ${TokenType.EQ}, got ${peek().type}`)
-        const initializer = this.__parseExpression()
+        const initializer = this.__parseExpression(peek().loc)
 
-        return new Declaration(ident, type ? new TypeExpr(type, initializer) : initializer)
+        return new Declaration(ident, type ? new TypeExpr(type, initializer, loc) : initializer, loc)
     }
-    this.parseWhileExpression = function(){
+    this.parseWhileExpression = function(loc){
         expect(TokenType.WHILE, `Expected ${TokenType.WHILE}, got ${peek().type}`)
-        const cond = this.parseLeftPrecedenceExpr()
+        const cond = this.parseLeftPrecedenceExpr(peek().loc)
         expect(TokenType.DO, `Expected ${TokenType.DO}, got ${peek().type}`)
-        const body = this.__parseExpression()
-        return new WhileExpr(cond, body)
+        const body = this.__parseExpression(peek().loc)
+        return new WhileExpr(cond, body, loc)
     }
-    this.parseBlockExpression = function(){
+    this.parseBlockExpression = function(loc){
         expect(TokenType.LBRACE, `Expected ${TokenType.LBRACE}, got ${peek().type}`)
         const exprs = []
         
         while (!match(TokenType.RBRACE) && !match(TokenType.END)){
-            exprs.push(this.__parseExpression())
+            exprs.push(this.__parseExpression(peek().loc))
 
             // TODO: insert literal with value null if final semicolon is present. See Task 6
             if (!match(TokenType.RBRACE) && !isBlock(exprs[exprs.length - 1])){
@@ -122,101 +122,101 @@ function Parser(tokens){
             }
         }
         expect(TokenType.RBRACE, `Missing ${TokenType.RBRACE} at ${peek().type}`)
-        return new Block(exprs)
+        return new Block(exprs, loc)
     }
-    this.parseAssignment = function(){
-        const expr = this.parseLeftPrecedenceExpr()
+    this.parseAssignment = function(loc){
+        const expr = this.parseLeftPrecedenceExpr(peek().loc)
 
         if (match(TokenType.EQ)){
             advance()
-            const value = this.__parseExpression()
+            const value = this.__parseExpression(peek().loc)
 
             if (expr instanceof Identifier){
-                return new Assignment(expr, value)
+                return new Assignment(expr, value, loc)
             }
             throw new Error('Invalid assignment target!')
         }
         return expr
     }
-    this.parseLeftPrecedenceExpr = function(){
-        return this.__parseLeftPrecedenceExpr(0, left_precedence_ops)
+    this.parseLeftPrecedenceExpr = function(loc){
+        return this.__parseLeftPrecedenceExpr(0, left_precedence_ops, loc)
     }
-    this.__parseLeftPrecedenceExpr = function(current_level, more){
+    this.__parseLeftPrecedenceExpr = function(current_level, more, loc){
         const next_level = current_level + 1
-        let left = next_level < more.length ? this.__parseLeftPrecedenceExpr(next_level, more) : this.parseUnaryExpression()
+        let left = next_level < more.length ? this.__parseLeftPrecedenceExpr(next_level, more, peek().loc) : this.parseUnaryExpression(peek().loc)
 
         while (match(...more[current_level].types)){
             const op = advance()
-            const right = next_level < more.length ? this.__parseLeftPrecedenceExpr(next_level, more) : this.parseUnaryExpression()
+            const right = next_level < more.length ? this.__parseLeftPrecedenceExpr(next_level, more, peek().loc) : this.parseUnaryExpression(peek().loc)
 
-            left = new more[current_level].produces(left, op.value, right)
+            left = new more[current_level].produces(left, op.value, right, loc)
         }
         return left
     }
-    this.parseUnaryExpression = function(){
+    this.parseUnaryExpression = function(loc){
         if (match(TokenType.MINUS, TokenType.NOT)){
             const op = advance()
-            const right = this.parseUnaryExpression()
-            return new UnaryExpr(right, op.value)
+            const right = this.parseUnaryExpression(peek().loc)
+            return new UnaryExpr(right, op.value, loc)
         }
-        return this.parseFunctionCall()
+        return this.parseFunctionCall(loc)
     }
-    this.parseFunctionCall = function(){
-        const expr = this.parseFactor()
+    this.parseFunctionCall = function(loc){
+        const expr = this.parseFactor(peek().loc)
 
         if (match(TokenType.LPAREN)){
             advance()
             const args = []
 
             if (!match(TokenType.RPAREN)){
-                args.push(this.__parseExpression())
+                args.push(this.__parseExpression(peek().loc))
 
                 while (match(TokenType.COMMA)){
                     advance()
-                    args.push(this.__parseExpression())
+                    args.push(this.__parseExpression(peek().loc))
                 }
             }
             expect(TokenType.RPAREN, `Expected ")" got ${peek().type}`)
-            return new Call(expr, args)
+            return new Call(expr, args, loc)
         }
         return expr
     }
-    this.parseFactor = function(){
+    this.parseFactor = function(loc){
         if (match(TokenType.LPAREN)){
-            return this.parseGroup()
+            return this.parseGroup(loc)
         } else if (match(TokenType.INT_LITERAL)){
-            return this.parseIntLiteral()
+            return this.parseIntLiteral(loc)
         } else if (match(TokenType.BOOL_LITERAL)){
-            return this.parseBoolLiteral()
+            return this.parseBoolLiteral(loc)
         } else if (match(TokenType.UNIT_LITERAL)){
-            return this.parseUnitLiteral()
+            return this.parseUnitLiteral(loc)
         } else if (match(TokenType.IDENTIFIER)){
-            return this.parseIdentifier()
+            return this.parseIdentifier(loc)
         }
         throw new Error(`Expected one of ${[TokenType.LPAREN, TokenType.INT_LITERAL, TokenType.BOOL_LITERAL, TokenType.UNIT_LITERAL, TokenType.IDENTIFIER].join(', ')} got ${peek().type} instead`)
     }
-    this.parseGroup = function(){
+    this.parseGroup = function(loc){
         expect(TokenType.LPAREN, `Expected "(" got ${peek().type}`)
-        const expr = this.__parseExpression()
+        const expr = this.__parseExpression(peek().loc)
         expect(TokenType.RPAREN, `Expected ")" got ${peek().type}`)
-        return new Grouping(expr)
+        return new Grouping(expr, loc)
     }
 
-    this.parseIdentifier = function(){
+    this.parseIdentifier = function(loc){
         const token = expect(TokenType.IDENTIFIER, `Expected identifier, got ${peek().type}`)
-        return new Identifier(token.value)
+        return new Identifier(token.value, loc)
     }
-    this.parseIntLiteral = function(){
+    this.parseIntLiteral = function(loc){
         const token = expect(TokenType.INT_LITERAL, `Expected an integer literal, got ${peek().type}`)
-        return new Literal(token.value)
+        return new Literal(token.value, loc)
     }
-    this.parseBoolLiteral = function(){
+    this.parseBoolLiteral = function(loc){
         const token = expect(TokenType.BOOL_LITERAL, `Expected a bool literal, got ${peek().type}`)
-        return new Literal(token.value === 'true')
+        return new Literal(token.value === 'true', loc)
     }
-    this.parseUnitLiteral = function(){
+    this.parseUnitLiteral = function(loc){
         expect(TokenType.UNIT_LITERAL, `Expected a unit literal, got ${peek().type}`)
-        return new Literal(null)
+        return new Literal(null, loc)
     }
 }
 
